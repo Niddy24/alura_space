21d04d9809ffd97de1d5ab2270817a05bd4bd2c3
Niddy24 <dcauan24@gmail.com>
2024-07-05 19:48:49 -0300

    adicionando banco de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+    1: {'nome': 'Nebulosa de Carina',
+        'legenda': 'webbtelescope.org / NASA / james Webb' 
+        },
+    2: {
+        'nome': 'Galaxia NGC 1079',
+        'legenda': 'NASA.org / NASA : Hunbble'
+        },
+            }
+    return render(request, 'galeria/index.html' , {'cards': dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
